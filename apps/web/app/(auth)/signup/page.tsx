64e3086f483ba8dab679a648e16cbc54f02309b2
Tanujeet <singhtanujeet@example.com>
2025-05-07 10:13:04 +0530

    auth page

---
+++ apps/web/app/(auth)/signup/page.tsx
@@ -0,0 +1,14 @@
+import Auth from "../../../Components/Auth";
+
+export const Signup = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div>
+          <Auth />
+        </div>
+        <div className="hidden lg:block"></div>
+      </div>
+    </div>
+  );
+};
