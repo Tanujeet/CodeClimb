caa087fbee2de1103c9a8f33d8f7c67b202db088
Tanujeet <singhtanujeet@gmail.com>
2025-05-21 08:48:51 +0530

    Home page frontend created

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -12,7 +12,7 @@ const page = () => {
         <h1 className="font-semibold text-[2.2rem] md:text-[3rem] lg:text-[4rem] max-w-[800px] mt-4 z-10">
           Your Simple Guide To <br />
           <span className="text-transparent bg-clip-text bg-gradient-to-r w-fit from-purple-500 via-pink-400 to-yellow-300">
-            Code projects
+            Code Projects
           </span>
         </h1>
         <p className="text-gray-200 max-w-[800px] text-md lg:text-lg block font-light mt-4">
