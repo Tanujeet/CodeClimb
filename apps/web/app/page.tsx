71fd00c6ab265fe9e7d0abfd66c8506ff6420fc5
Tanujeet <singhtanujeet@gmail.com>
2025-05-21 08:29:24 +0530

    Home page frontend created

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import Navbar from "../Components/Navbar";
+import { useRouter } from "next/navigation";
 
 const page = () => {
+  const router = useRouter();
   return (
     <div>
       <Navbar />
@@ -14,6 +18,12 @@ const page = () => {
         <p className="text-gray-200 max-w-[800px] text-md lg:text-lg block font-light mt-4">
           Climb from beginner to pro with real-world coding challenges.
         </p>
+        <button
+          onClick={() => router.push("/auth/signin")}
+          className="relative mt-5 px-6 py-3 rounded-xl text-xs bg-slate-800 w-fit h-fit  outline outline-offset-2 outline-slate-800 hover:outline-purple-500 font-semibold hover:bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 glass-effect transition-all duration-300 ease-in-out"
+        >
+          Improve your Coding skills
+        </button>
       </div>
     </div>
   );
