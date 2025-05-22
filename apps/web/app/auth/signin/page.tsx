a99e342060c9f14da0cc13476dbac038fa966980
Tanujeet <singhtanujeet@gmail.com>
2025-05-22 20:52:45 +0530

    signin page added

--- apps/web/app/auth/signin/page.tsx
+++ apps/web/app/auth/signin/page.tsx
@@ -1,18 +1,32 @@
 const Signin = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <div className="w-full max-w-md p-8 rounded-2xl shadow-lg ">
+      <div className="w-full max-w-md p-8 border border-white rounded-2xl shadow-lg ">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign In</h2>
         <form className="space-y-4">
           <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
+            <label htmlFor="email" className="block text-sm font-medium">
               Email
             </label>
-            <input type="email" id="email" className="" placeholder="" />
+            <input
+              type="email"
+              id="email"
+              className="mt-1 block w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500"
+              placeholder=""
+            />
           </div>
+          <div>
+            <label htmlFor="password" className="block tex-sm font font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder=""
+              id="password"
+              className="mt-1 block w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <button></button>
         </form>
       </div>
     </div>
