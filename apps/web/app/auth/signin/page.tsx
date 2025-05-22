12a26dd517024cae0f1f767775981b8667ba665d
Tanujeet <singhtanujeet@gmail.com>
2025-05-22 20:39:45 +0530

    signin page added

--- apps/web/app/auth/signin/page.tsx
+++ apps/web/app/auth/signin/page.tsx
@@ -1,5 +1,22 @@
 const Signin = () => {
-  return <div className="bg-white"></div>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="w-full max-w-md p-8 rounded-2xl shadow-lg ">
+        <h2 className="text-2xl font-bold mb-6 text-center">Sign In</h2>
+        <form className="space-y-4">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input type="email" id="email" className="" placeholder="" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
