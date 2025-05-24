3a66d4d75877678c5b6e353787ce7355cb4eccc1
Tanujeet <singhtanujeet@gmail.com>
2025-05-24 07:54:53 +0530

    queries for aut

---
+++ apps/web/app/api/signin/route.ts
@@ -0,0 +1,44 @@
+// app/api/signup/route.ts
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: Request) {
+  try {
+    const { email, password,  } = await req.json();
+
+
+    if (!email || !password ||)  {
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 409 }
+      );
+    }
+
+  
+    const newUser = await prisma.user.create({
+      data: { email, password, }, 
+    });
+
+    return NextResponse.json(
+      { message: "User created successfully", user: newUser },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
