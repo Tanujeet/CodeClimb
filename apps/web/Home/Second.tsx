b296879653eb2ec76e7bd10d2c15d0bb7730de34
Tanujeet <singhtanujeet@gmail.com>
2025-05-26 08:55:56 +0530

    created a new folder for home page

---
+++ apps/web/Home/Second.tsx
@@ -0,0 +1,9 @@
+const Second = () => {
+  return (
+      <div className="h-screen w-screen flex justify-center">
+          <div className="mt-10">hy</div>
+      </div>
+  );
+};
+
+export default Second;
