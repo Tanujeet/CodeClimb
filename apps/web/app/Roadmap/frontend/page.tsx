6abd20f7736224553d9f64afa3a015798f748b6c
Tanujeet <singhtanujeet@gmail.com>
2025-05-31 08:12:01 +0530

    career Roadmap changes

--- apps/web/app/Roadmap/frontend/page.tsx
+++ apps/web/app/Roadmap/frontend/page.tsx
@@ -1,17 +1,25 @@
+import Button from "../../../Components/Button";
+import Navbar from "../../../Components/Navbar";
+
 const page = () => {
-    return (
-      <div className="min-h-screen w-screen flex justify-center">
-        <div className="mt-40">
-          <h1 className="uppercase text-5xl text-center font-bold">
-            How To Become a <br />
-            Front End Developer
-          </h1>
-          <p className="text-center text-lg mt-4 text-gray-600">
-            Step-by-step guide to mastering frontend skills in 2025.
-          </p>
+  return (
+    <div className="min-h-screen w-screen flex justify-center">
+      <Navbar />
+
+      <div className="mt-40">
+        <h1 className="uppercase text-5xl text-center font-bold">
+          How To Become a <br />
+          Front End Developer
+        </h1>
+        <p className="text-center text-lg mt-4 text-gray-600">
+          Step-by-step guide to mastering frontend skills in 2025.
+        </p>
+        <div className="ml-45 mt-5">
+          <Button title="Get started now" />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default page;
