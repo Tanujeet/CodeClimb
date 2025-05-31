a846bdc488814f18c994bd8acd952336073b6d9b
Tanujeet <singhtanujeet@gmail.com>
2025-05-31 08:22:58 +0530

    career Roadmap changes

--- apps/web/app/Roadmap/frontend/page.tsx
+++ apps/web/app/Roadmap/frontend/page.tsx
@@ -3,22 +3,23 @@ import Navbar from "../../../Components/Navbar";
 
 const page = () => {
   return (
-    <div className="min-h-screen w-screen flex justify-center">
+    <section className="h-screen w-screen flex justify-center">
       <Navbar />
-
-      <div className="mt-40">
-        <h1 className="uppercase text-5xl text-center font-bold">
-          How To Become a <br />
-          Front End Developer
-        </h1>
-        <p className="text-center text-lg mt-4 text-gray-600">
-          Step-by-step guide to mastering frontend skills in 2025.
-        </p>
-        <div className="ml-45 mt-5">
-          <Button title="Get started now" />
+      <div className="relative mt-56 lg:mt-50 ">
+        <div className="relative flex flex-col gap-6 z-10 justify-center items-center text-center0">
+          <h1 className="uppercase text-5xl text-center font-bold">
+            How To Become a <br />
+            Front End Developer
+          </h1>
+          <p className="text-gray-400 max-w-[600px] text-md lg:text-lg block font-light">
+            Step-by-step guide to mastering frontend skills in 2025.
+          </p>
+          <div className="mt-4">
+            <Button title="Get started now" />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
