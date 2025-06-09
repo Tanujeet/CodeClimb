6abf6c783cb2ad4448a2d33ccc332c3aeb30276a
Tanujeet <singhtanujeet@gmail.com>
2025-06-09 08:07:49 +0530

    implemetned blog page 1

--- apps/web/app/blogs/1/page.tsx
+++ apps/web/app/blogs/1/page.tsx
@@ -1,6 +1,16 @@
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <section className="px-2 lg:px-[4rem] text-gray-200 py-[4rem] mt-4 ">
+        <div className="relative flex flex-col gap-2 lg:gap-6 z-10 justify-center items-center text-center">
+          <h3 className="font-semibold text-center !text-[2rem] md:text-[2.4rem] lg:text-[3rem] max-w-[800px] z-10">
+            “How to Use AI Tools like GitHub Copilot Efficiently <br />
+            (Without Getting Lazy)”
+          </h3>
+          <p className=""></p>
+        </div>
+      </section>
+    </main>
   );
 };
 
