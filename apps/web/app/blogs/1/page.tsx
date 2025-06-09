35036796d366ecb88f0eaa92c75647eefba6834a
Tanujeet <singhtanujeet@gmail.com>
2025-06-09 08:08:41 +0530

    implemetned blog page 1

--- apps/web/app/blogs/1/page.tsx
+++ apps/web/app/blogs/1/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
   return (
     <main>
-      <section className="px-2 lg:px-[4rem] text-gray-200 py-[4rem] mt-4 ">
+      <section className="px-2 lg:px-[4rem] text-black py-[4rem] mt-15 ">
         <div className="relative flex flex-col gap-2 lg:gap-6 z-10 justify-center items-center text-center">
           <h3 className="font-semibold text-center !text-[2rem] md:text-[2.4rem] lg:text-[3rem] max-w-[800px] z-10">
             “How to Use AI Tools like GitHub Copilot Efficiently <br />
